3ef8079c7a11260c8be85055657794ed2475c89c
Peter Dotterweich <58152012+PeterD5353@users.noreply.github.com>
2024-03-04 22:59:18 +0000

    create file for gui

---
+++ GUI.py
@@ -0,0 +1,22 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("Dark")  
+customtkinter.set_default_color_theme("green")
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("960x540")
+app.title("Workout Playlist Generator")
+
+
+def make_playlist_pushed():
+    print("OK")
+
+
+
+
+
+
+
+
+# run app 
+app.mainloop()
